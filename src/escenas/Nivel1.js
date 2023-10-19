4d59d040346e5f06374066378c5b5b8a52ec5bf4
Joaset <jtf1wft@gmail.com>
2023-10-19 11:47:47 -0300

    agregando seleccion de personaje y aumento del puntaje para pasar de escena Nivel3 a escena Boss

--- src/escenas/Nivel1.js
+++ src/escenas/Nivel1.js
@@ -5,6 +5,10 @@ class Nivel1 extends Phaser.Scene {
         this.puntaje = 0;
     }
 
+    init(data){
+        this.numJugador = data.numJugador;
+    }
+
     preload() {
         this.load.image('sky', '../../public/img/background1.png')
         this.load.image('enemy', '/public/img/enemy.png')
@@ -13,6 +17,8 @@ class Nivel1 extends Phaser.Scene {
         this.load.image('item', '/public/img/shoot4.png')
         this.load.image('pared', '/public/img/pipe.png')
         this.load.spritesheet('sega', '/public/img/nave4.png', { frameWidth: 60, frameHeight: 56 })
+        this.load.spritesheet('nave', '/public/img/nave.png', { frameWidth: 70, frameHeight: 62 })
+        this.load.spritesheet('explosion', '/public/img/explosion.png', { frameWidth: 64, frameHeight: 64 })
         this.load.audio('fondo', '../public/sound/menu.mp3');
         this.load.audio('laser', '../public/sound/blaster.mp3');
         this.load.audio('muerteEnemigo', '../public/sound/alien_death.wav');
@@ -39,22 +45,87 @@ class Nivel1 extends Phaser.Scene {
         this.reload = true;
         this.balas = this.physics.add.group();
         this.bala;
+        console.log(this.numJugador);
 
         this.skyline = this.add.blitter(0, 0, 'sky');
         this.skyline.create(0, 0);
         this.skyline.create(800, 0);
 
-        this.flame = this.add.particles(0, 0, 'white',
-        {
-            color: [ 0x96e0da, 0x937ef3 ],
-            colorEase: 'quad.out',
-            lifespan: 1000,
-            angle: { min: 175, max: 185 },
-            scale: { start: 0.40, end: 0, ease: 'sine.out' },
-            speed: 220,
-            advance: 2000,
-            blendMode: 'ADD'
-        });
+        if (this.numJugador == 1) {
+            this.player = this.physics.add.sprite(100, 200, 'nave');
+            this.player.setCollideWorldBounds(true);
+
+            this.flame = this.add.particles(0, 0, 'white',
+            {
+                color: [ 0xfacc22, 0xf89800, 0xf83600, 0x9f0404 ],
+                colorEase: 'quad.out',
+                lifespan: 1000,
+                angle: { min: 175, max: 185 },
+                scale: { start: 0.40, end: 0, ease: 'sine.out' },
+                speed: 200,
+                advance: 2000,
+                blendMode: 'ADD'
+            });
+
+            this.flame.startFollow(this.player,-20,0);
+            
+            this.anims.create({
+                key: 'turn',
+                frames: [{ key: 'nave', frame: 0 }],
+                frameRate: 20
+            })
+            this.anims.create({
+                key: 'up',
+                frames: [{ key: 'nave', frame: 2 }],
+                frameRate: 10
+            })
+            this.anims.create({
+                key: 'down',
+                frames: [{ key: 'nave', frame: 1 }],
+                frameRate: 10
+            })
+        }
+
+        if (this.numJugador == 2) {
+
+            this.flame = this.add.particles(0, 0, 'white',
+            {
+                color: [ 0x96e0da, 0x937ef3 ],
+                colorEase: 'quad.out',
+                lifespan: 1000,
+                angle: { min: 175, max: 185 },
+                scale: { start: 0.40, end: 0, ease: 'sine.out' },
+                speed: 220,
+                advance: 2000,
+                blendMode: 'ADD'
+            });
+
+            this.player = this.physics.add.sprite(this.game.config.width / 8, this.game.config.height / 2, 'sega');
+            this.player.setCollideWorldBounds(true);
+            this.flame.startFollow(this.player,-25,0);
+
+            this.anims.create({
+                key: 'segaturn',
+                frames: [{ key: 'sega', frame: 0 }],
+                frameRate: 20
+            })
+            this.anims.create({
+                key: 'segaup',
+                frames: [{ key: 'sega', frame: 1 }],
+                frameRate: 10
+            })
+            this.anims.create({
+                key: 'segadown',
+                frames: [{ key: 'sega', frame: 2 }],
+                frameRate: 10
+            })
+        }
+
+        // this.anims.create({
+        //     key: 'explosionAnimacion',
+        //     frames: this.anims.generateFrameNames('explosion', {star: 0, end:24}),
+        //     frameRate: 10,
+        // })
 
 
         // se crean paredes para eliminar elementos fuera del mundo
@@ -62,27 +133,6 @@ class Nivel1 extends Phaser.Scene {
         this.paredes.create(-100, this.game.config.height / 2, 'pared').setScale(2).refreshBody();
         this.paredes.create(this.game.config.width + 200, this.game.config.height / 2, 'pared').setScale(2).refreshBody();
 
-
-        this.player = this.physics.add.sprite(this.game.config.width / 8, this.game.config.height / 2, 'sega');
-        this.player.setCollideWorldBounds(true);
-        this.flame.startFollow(this.player,-25,0);
-
-        // para el movimiento de la nave player (animaciones)
-        this.anims.create({
-            key: 'turn',
-            frames: [{ key: 'sega', frame: 0 }],
-            frameRate: 20
-        })
-        this.anims.create({
-            key: 'up',
-            frames: [{ key: 'sega', frame: 1 }],
-            frameRate: 10
-        })
-        this.anims.create({
-            key: 'down',
-            frames: [{ key: 'sega', frame: 2 }],
-            frameRate: 10
-        })
 
         // para detectar las entradas de teclado
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -98,8 +148,9 @@ class Nivel1 extends Phaser.Scene {
         })
 
         this.physics.add.collider(this.balas, this.paredes, this.outBullet, null, this);
-        this.puntajeText = this.add.text(16, 16, 'Puntaje: ' + this.puntaje + '/250', { fontSize: '32px', fill: '#fff' })
-        this.vidaText = this.add.text(16, 50, 'Vida: ' + this.vida + '%', { fontSize: '32px', fill: '#fff' })
+        this.puntajeText = this.add.text(16, 16, 'Puntaje: ' + this.puntaje + '/250', { fontSize: '32px', fill: '#fff' });
+        this.vidaText = this.add.text(16, 50, 'Vida: ' + this.vida + '%', { fontSize: '32px', fill: '#fff' });
+        // console.log(this.numJugador);
 
     }
 
@@ -107,27 +158,78 @@ class Nivel1 extends Phaser.Scene {
         this.skyline.x -= 1;
         this.skyline.x %= -800;
 
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-400);
-            this.player.anims.play('turn');
+        if (this.numJugador == 1) {
+            if (this.cursors.left.isDown) {
+                this.player.setVelocityX(-400);
+                this.player.anims.play('turn');
+            }
+            else if (this.cursors.right.isDown) {
+                this.player.setVelocityX(400);
+                this.player.anims.play('turn');
+            }
+            else if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-500);
+                this.player.anims.play('up')
+            }
+            else if (this.cursors.down.isDown) {
+                this.player.setVelocityY(500)
+                this.player.anims.play('down')
+            }
+            else {
+                this.player.setVelocityY(0);
+                this.player.setVelocityX(0);
+                this.player.anims.play('turn', true)
+            }
+            
         }
-        else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(400);
-            this.player.anims.play('turn');
+
+        if (this.numJugador == 2) {
+
+            if (this.cursors.left.isDown) {
+                this.player.setVelocityX(-400);
+                this.player.anims.play('segaturn');
+            }
+            else if (this.cursors.right.isDown) {
+                this.player.setVelocityX(400);
+                this.player.anims.play('segaturn');
+            }
+            else if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-500);
+                this.player.anims.play('segaup')
+            }
+            else if (this.cursors.down.isDown) {
+                this.player.setVelocityY(500)
+                this.player.anims.play('segadown')
+            }
+            else {
+                this.player.setVelocityY(0);
+                this.player.setVelocityX(0);
+                this.player.anims.play('segaturn', true)
+            }
+            
         }
-        else if (this.cursors.up.isDown) {
-            this.player.setVelocityY(-500);
-            this.player.anims.play('up')
-        }
-        else if (this.cursors.down.isDown) {
-            this.player.setVelocityY(500)
-            this.player.anims.play('down')
-        }
-        else {
-            this.player.setVelocityY(0);
-            this.player.setVelocityX(0);
-            this.player.anims.play('turn', true)
-        }
+
+        // if (this.cursors.left.isDown) {
+        //     this.player.setVelocityX(-400);
+        //     this.player.anims.play('turn');
+        // }
+        // else if (this.cursors.right.isDown) {
+        //     this.player.setVelocityX(400);
+        //     this.player.anims.play('turn');
+        // }
+        // else if (this.cursors.up.isDown) {
+        //     this.player.setVelocityY(-500);
+        //     this.player.anims.play('up')
+        // }
+        // else if (this.cursors.down.isDown) {
+        //     this.player.setVelocityY(500)
+        //     this.player.anims.play('down')
+        // }
+        // else {
+        //     this.player.setVelocityY(0);
+        //     this.player.setVelocityX(0);
+        //     this.player.anims.play('turn', true)
+        // }
         // cuando se pulse la tecla 32(espacio) la nave dispara
         this.input.keyboard.on('keydown', (event) => {
             if (event.keyCode == 32 && this.reload) {
@@ -178,12 +280,10 @@ class Nivel1 extends Phaser.Scene {
 
     outBullet(balas) {
         balas.destroy();
-        //console.log('se elimino la bala')
     }
 
     outEnemy(enemy) {
         enemy.destroy();
-        //console.log('se elimino el enemigo')
     }
 
     hitenemy(player, enemy) {
@@ -208,7 +308,6 @@ class Nivel1 extends Phaser.Scene {
     }
 
     hitbullet(enemy, balas) {
-        // console.log("funca");
         this.puntaje += 10;
         balas.destroy();
         enemy.destroy();
@@ -223,7 +322,7 @@ class Nivel1 extends Phaser.Scene {
                 amount: 40,
                 onComplete: () => {
                     this.cameras.main.fadeOut(100);
-                    this.scene.start("Nivel2", { puntaje: this.puntaje , vida: this.vida, sonido: this.sonido});
+                    this.scene.start("Nivel2", { puntaje: this.puntaje , vida: this.vida, sonido: this.sonido, numJugador:this.numJugador});
                 }
             })
         }
@@ -239,7 +338,6 @@ class Nivel1 extends Phaser.Scene {
     }
 
     obtenerPowerup() {
-        console.log('powerup agarrado');
         this.vida += 100;
         this.vidaJugador = this.sound.add('vida', {volume: 0.1});
         this.vidaJugador.play();
--- src/escenas/Nivel3.js
+++ src/escenas/Nivel3.js
@@ -9,6 +9,7 @@ class Nivel3 extends Phaser.Scene {
         this.puntaje = data.puntaje;
         this.vida = data.vida;
         this.sonido = data.sonido;
+        this.numJugador = data.numJugador;
     }
     preload() {
         this.load.image('space2', '../../public/img/fondo-space-1.PNG')
@@ -18,7 +19,8 @@ class Nivel3 extends Phaser.Scene {
         this.load.image('shootenemy', '/public/img/shootEnemy.png')
         this.load.image('pared', '/public/img/pipe.png')
         this.load.image('white','/public/img/white.png')
-        this.load.spritesheet('nave', '/public/img/nave4.png', { frameWidth: 60, frameHeight: 56 })
+        this.load.spritesheet('sega', '/public/img/nave4.png', { frameWidth: 60, frameHeight: 56 })
+        this.load.spritesheet('nave', '/public/img/nave.png', { frameWidth: 70, frameHeight: 62 });
         this.load.audio('laser', '../public/sound/blaster.mp3');
         this.load.audio('muerteEnemigo', '../public/sound/alien_death.wav');
         this.load.audio('muerte', '../public/sound/player_death.wav');
@@ -32,48 +34,87 @@ class Nivel3 extends Phaser.Scene {
         this.balas = this.physics.add.group();
         this.balasEnemy = this.physics.add.group();
         this.bala;
+        console.log(this.numJugador);
 
         this.skyline = this.add.blitter(0, 0, 'space2');
         this.skyline.create(0, 0);
         this.skyline.create(800, 0);
 
-        this.flame = this.add.particles(0, 0, 'white',
-        {
-            color: [ 0x96e0da, 0x937ef3 ],
-            colorEase: 'quad.out',
-            lifespan: 1000,
-            angle: { min: 175, max: 185 },
-            scale: { start: 0.40, end: 0, ease: 'sine.out' },
-            speed: 220,
-            advance: 2000,
-            blendMode: 'ADD'
-        });
+        if (this.numJugador == 1) {
+
+            this.player = this.physics.add.sprite(100, 200, 'nave');
+            this.player.setCollideWorldBounds(true);
+
+            this.flame = this.add.particles(0, 0, 'white',
+            {
+                color: [ 0xfacc22, 0xf89800, 0xf83600, 0x9f0404 ],
+                colorEase: 'quad.out',
+                lifespan: 1000,
+                angle: { min: 175, max: 185 },
+                scale: { start: 0.40, end: 0, ease: 'sine.out' },
+                speed: 200,
+                advance: 2000,
+                blendMode: 'ADD'
+            });
+
+            this.flame.startFollow(this.player,-20,0);
+            
+            this.anims.create({
+                key: 'naveturn',
+                frames: [{ key: 'nave', frame: 0 }],
+                frameRate: 20
+            })
+            this.anims.create({
+                key: 'naveup',
+                frames: [{ key: 'nave', frame: 2 }],
+                frameRate: 10
+            })
+            this.anims.create({
+                key: 'navedown',
+                frames: [{ key: 'nave', frame: 1 }],
+                frameRate: 10
+            })
+            
+        }
+        if (this.numJugador == 2) {
+
+            this.flame = this.add.particles(0, 0, 'white',
+            {
+                color: [ 0x96e0da, 0x937ef3 ],
+                colorEase: 'quad.out',
+                lifespan: 1000,
+                angle: { min: 175, max: 185 },
+                scale: { start: 0.40, end: 0, ease: 'sine.out' },
+                speed: 220,
+                advance: 2000,
+                blendMode: 'ADD'
+            });
+
+            this.player = this.physics.add.sprite(100, 200, 'sega');
+            this.player.setCollideWorldBounds(true);
+            this.flame.startFollow(this.player,-25,0);
+
+            this.anims.create({
+                key: 'segaturn1',
+                frames: [{ key: 'sega', frame: 0 }],
+                frameRate: 20
+            })
+            this.anims.create({
+                key: 'segaup1',
+                frames: [{ key: 'sega', frame: 1 }],
+                frameRate: 10
+            })
+            this.anims.create({
+                key: 'segadown1',
+                frames: [{ key: 'sega', frame: 2 }],
+                frameRate: 10
+            })
+        }
 
         // se crean paredes para eliminar elementos fuera del mundo
         this.paredes = this.physics.add.staticGroup();
         this.paredes.create(-100, this.game.config.height / 2, 'pared').setScale(2).refreshBody();
         this.paredes.create(this.game.config.width + 200, this.game.config.height / 2, 'pared').setScale(2).refreshBody();
-
-        this.player = this.physics.add.sprite(100, 200, 'nave');
-        this.player.setCollideWorldBounds(true);
-        this.flame.startFollow(this.player,-25,0);
-
-        // para el movimiento de la nave player
-        this.anims.create({
-            key: 'turn',
-            frames: [{ key: 'nave', frame: 0 }],
-            frameRate: 20
-        })
-        this.anims.create({
-            key: 'up',
-            frames: [{ key: 'nave', frame: 1 }],
-            frameRate: 10
-        })
-        this.anims.create({
-            key: 'down',
-            frames: [{ key: 'nave', frame: 2 }],
-            frameRate: 10
-        })
 
         // para detectar las entradas de teclado
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -102,7 +143,7 @@ class Nivel3 extends Phaser.Scene {
         })
 
         this.physics.add.collider(this.balas, this.paredes, this.outBullet, null, this);
-        this.puntajeText = this.add.text(16, 40, 'Puntaje: ' + this.puntaje + '/1500', { fontSize: '32px', fill: '#fff' })
+        this.puntajeText = this.add.text(16, 40, 'Puntaje: ' + this.puntaje + '/2000', { fontSize: '32px', fill: '#fff' })
         this.vidaText = this.add.text(16, 16, 'Vida: ' + this.vida + '%', { fontSize: '32px', fill: '#fff' })
     }
 
@@ -111,27 +152,79 @@ class Nivel3 extends Phaser.Scene {
         this.skyline.x %= -800;
 
         this.hitCheck = true;
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-400);
-            this.player.anims.play('turn');
+
+        if (this.numJugador == 1) {
+
+            if (this.cursors.left.isDown) {
+                this.player.setVelocityX(-400);
+                this.player.anims.play('naveturn');
+            }
+            else if (this.cursors.right.isDown) {
+                this.player.setVelocityX(400);
+                this.player.anims.play('naveturn');
+            }
+            else if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-500);
+                this.player.anims.play('naveup')
+            }
+            else if (this.cursors.down.isDown) {
+                this.player.setVelocityY(500)
+                this.player.anims.play('navedown')
+            }
+            else {
+                this.player.setVelocityY(0);
+                this.player.setVelocityX(0);
+                this.player.anims.play('naveturn', true)
+            }
+            
         }
-        else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(400);
-            this.player.anims.play('turn');
+
+        if (this.numJugador == 2) {
+
+            if (this.cursors.left.isDown) {
+                this.player.setVelocityX(-400);
+                this.player.anims.play('segaturn1');
+            }
+            else if (this.cursors.right.isDown) {
+                this.player.setVelocityX(400);
+                this.player.anims.play('segaturn1');
+            }
+            else if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-500);
+                this.player.anims.play('segaup1')
+            }
+            else if (this.cursors.down.isDown) {
+                this.player.setVelocityY(500)
+                this.player.anims.play('segadown1')
+            }
+            else {
+                this.player.setVelocityY(0);
+                this.player.setVelocityX(0);
+                this.player.anims.play('segaturn1', true)
+            }   
         }
-        else if (this.cursors.up.isDown) {
-            this.player.setVelocityY(-500);
-            this.player.anims.play('up')
-        }
-        else if (this.cursors.down.isDown) {
-            this.player.setVelocityY(500)
-            this.player.anims.play('down')
-        }
-        else {
-            this.player.setVelocityY(0);
-            this.player.setVelocityX(0);
-            this.player.anims.play('turn', true)
-        }
+
+        // if (this.cursors.left.isDown) {
+        //     this.player.setVelocityX(-400);
+        //     this.player.anims.play('turn');
+        // }
+        // else if (this.cursors.right.isDown) {
+        //     this.player.setVelocityX(400);
+        //     this.player.anims.play('turn');
+        // }
+        // else if (this.cursors.up.isDown) {
+        //     this.player.setVelocityY(-500);
+        //     this.player.anims.play('up')
+        // }
+        // else if (this.cursors.down.isDown) {
+        //     this.player.setVelocityY(500)
+        //     this.player.anims.play('down')
+        // }
+        // else {
+        //     this.player.setVelocityY(0);
+        //     this.player.setVelocityX(0);
+        //     this.player.anims.play('turn', true)
+        // }
         // cuando se pulse la tecla 32(espacio) el bird da n salto hacia arriba
         this.input.keyboard.on('keydown', (event) => {
             if (event.keyCode == 32 && this.reload) {
@@ -183,21 +276,17 @@ class Nivel3 extends Phaser.Scene {
 
     outBullet(balas) {
         balas.destroy();
-        //console.log('se elimino la bala')
     }
 
     outEnemy(enemy) {
         enemy.destroy();
-        //console.log('se elimino el enemigo')
     }
 
     outHealer(healer) {
         healer.destroy();
-        //console.log('se alimino el healer')
     }
     outShooter(healer) {
         healer.destroy();
-        //console.log('se alimino el shooter')
     }
 
     createHealer() {
@@ -251,9 +340,7 @@ class Nivel3 extends Phaser.Scene {
         this.vidaJugador = this.sound.add('vida', {volume: 0.1});
         this.vidaJugador.play();
         if (this.vida < 100) {
-            console.log("te curaste boludo qliao")
             this.healCount += 1;
-            console.log(this.healCount)
             this.vida = 100;
             this.vidaText.setText('Vida: ' + this.vida + '%');
             this.player.setTint(0x90ee90);
@@ -267,10 +354,7 @@ class Nivel3 extends Phaser.Scene {
             })
         }
     }
- /*   collideBullet(balas, balasEnemy) {
-        balasEnemy.destroy();
-        balas.destroy();
-    }*/
+
     hitShooter(player, shooter) {
         shooter.destroy();
         this.vida -= 25;
@@ -339,8 +423,8 @@ class Nivel3 extends Phaser.Scene {
         enemy.destroy();
         this.muerteEnemigo = this.sound.add('muerteEnemigo', {volume: 0.1});
         this.muerteEnemigo.play();
-        this.puntajeText.setText("Puntaje: " + this.puntaje + "/1500");
-        if (this.puntaje >= 1500) {
+        this.puntajeText.setText("Puntaje: " + this.puntaje + "/2000");
+        if (this.puntaje >= 2000) {
             this.sonido.stop();
             const pixelated = this.cameras.main.postFX.addPixelate(-1);
             this.add.tween({
@@ -349,7 +433,7 @@ class Nivel3 extends Phaser.Scene {
                 amount: 40,
                 onComplete: () => {
                     this.cameras.main.fadeOut(100);
-                    this.scene.start('Boss', { puntaje: this.puntaje, vida: this.vida });
+                    this.scene.start('Boss', { puntaje: this.puntaje, vida: this.vida, numJugador: this.numJugador});
                 }
             })
         }
@@ -361,8 +445,8 @@ class Nivel3 extends Phaser.Scene {
         shooter.destroy();
         this.muerteEnemigo = this.sound.add('muerteEnemigo', {volume: 0.1});
         this.muerteEnemigo.play();
-        this.puntajeText.setText("Puntaje: " + this.puntaje + "/1500");
-        if (this.puntaje >= 1500) {
+        this.puntajeText.setText("Puntaje: " + this.puntaje + "/2000");
+        if (this.puntaje >= 2000) {
             this.sonido.stop();
             const pixelated = this.cameras.main.postFX.addPixelate(-1);
             this.add.tween({
@@ -371,7 +455,7 @@ class Nivel3 extends Phaser.Scene {
                 amount: 40,
                 onComplete: () => {
                     this.cameras.main.fadeOut(100);
-                    this.scene.start('Boss', { puntaje: this.puntaje, vida: this.vida });
+                    this.scene.start('Boss', { puntaje: this.puntaje, vida: this.vida, numJugador: this.numJugador});
                 }
             })
         }
